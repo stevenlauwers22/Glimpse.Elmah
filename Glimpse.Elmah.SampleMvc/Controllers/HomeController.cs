07562675804807e56c3e545fae068f8f14265f4f
steven.lauwers <steven.lauwers@medicim.com>
2017-02-03 14:17:46 +0100

    Added an MVC sample project

---
+++ Glimpse.Elmah.SampleMvc/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace Glimpse.Elmah.SampleMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var exceptionType = (typeof(Exception));
+            var exceptionTypes = Assembly
+                .GetAssembly(exceptionType)
+                .GetTypes()
+                .Where(t => t.IsSubclassOf(exceptionType))
+                .ToList();
+            
+            return View(exceptionTypes);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string exceptionTypeToThrow)
+        {
+            var exceptionType = Type.GetType(exceptionTypeToThrow);
+            if (exceptionType == null)
+                return View();
+
+            var exception = (Exception)Activator.CreateInstance(exceptionType);
+            throw exception;
+        }
+    }
+}
---
+++ Glimpse.Elmah.SampleMvc/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Routing;
+using Glimpse.Elmah.SampleMvc.App_Start;
+
+namespace Glimpse.Elmah.SampleMvc
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
